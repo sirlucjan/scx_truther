83c841b74a7f98c16c805155f7e4378e4ea4d97c
Piotr Gorski <lucjan.lucjanov@gmail.com>
2025-12-19 21:44:26 +0100

    cargo: fix formatting
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use env_logger::Env;
 use scx_truther::scheduler::TrutherScheduler;
 
 fn main() {
-    env_logger::Builder::from_env(
-        Env::default().default_filter_or("info"),
-    ).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     TrutherScheduler::run();
 }
--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 pub fn seed_from_system() -> u64 {
     SystemTime::now()
-    .duration_since(UNIX_EPOCH)
-    .map(|d| d.as_nanos() as u64)
-    .unwrap_or(0)
+        .duration_since(UNIX_EPOCH)
+        .map(|d| d.as_nanos() as u64)
+        .unwrap_or(0)
 }
