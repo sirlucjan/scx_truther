c9abdad8190a0160e1732f9998a8567a194a8fbc
Piotr Gorski <lucjan.lucjanov@gmail.com>
2026-07-20 08:44:01 +0200

    scx_truther: update tests
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- tests/truth.rs
+++ tests/truth.rs
@@ -1,9 +1,28 @@
-use scx_truther::truth::Truth;
+use scx_truther::astrology::Astrology;
+use scx_truther::numerology::LifePath;
+use scx_truther::util::Rng;
 
 #[test]
-fn zodiac_is_stable_for_seed() {
-    let t1 = Truth::new(42);
-    let t2 = Truth::new(42);
+fn astrology_is_deterministic_for_seed() {
+    let a1 = Astrology::from_seed(42);
+    let a2 = Astrology::from_seed(42);
 
-    assert_eq!(t1.index(), t2.index());
+    assert_eq!(a1.index(), a2.index());
+}
+
+#[test]
+fn rng_is_deterministic_for_seed() {
+    let mut r1 = Rng::new(1234);
+    let mut r2 = Rng::new(1234);
+
+    assert_eq!(r1.next_u64(), r2.next_u64());
+    assert_eq!(r1.next_u64(), r2.next_u64());
+}
+
+#[test]
+fn life_path_preserves_master_numbers() {
+    // 2 + 9 = 11, a master number that should not be reduced further.
+    assert_eq!(LifePath::from_pid(29).number(), 11);
+    // 4 + 4 = 8, an ordinary number that should reduce normally.
+    assert_eq!(LifePath::from_pid(44).number(), 8);
 }
